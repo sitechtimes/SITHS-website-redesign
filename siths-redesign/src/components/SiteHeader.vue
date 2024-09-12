ef372a9da15723b2ff25794fe0e4b9f7ee2188e5
andreaguo <113132239+andreaguo@users.noreply.github.com>
2024-09-12 11:36:07 -0400

    component done in clubs page

--- siths-redesign/src/components/SiteHeader.vue
+++ siths-redesign/src/components/SiteHeader.vue
@@ -2,14 +2,14 @@
   <div class="flex flex-row justify-between">
     <img class="m-4" src="../assets/icons/menu-bar.png" alt="hamburger icon" @click="toggleVisibility">
     <div class="p-4 flex flex-row text-center justify-center items-center">
-      <router-link :to="{name: 'home'}" class="text-3xl text-white">SITECH</router-link>
+      <router-link :to="{name: 'home'}" class="text-3xl text-white" @click="isVisible = false">SITECH</router-link>
     </div>
     <div class="p-4">SEARCH</div>
   </div>
 
   <!-- navigation menu -->
   <div ref="menu" id="nav" class="absolute bg-[#272525] h-screen w-full flex items-center justify-center">
-    <RouterLink to="/" class="text-3xl text-white">Home</RouterLink>
+    <RouterLink :to="{name: 'clubs'}" class="text-3xl text-white">Clubs and Activities</RouterLink>
   </div>
 </template>
 
@@ -29,6 +29,7 @@ const toggleVisibility = () => {
       duration: 1,
       ease: 'power4.in'
     })
+    
   } else {
     // Animate menu in
     gsap.fromTo(menu.value, { opacity: 1, y: -1000 }, { opacity: 1, y: 2, duration: 1.5, ease: 'power4.out' })
--- siths-redesign/src/components/SubpageMenu.vue
+++ siths-redesign/src/components/SubpageMenu.vue
@@ -1,7 +1,7 @@
 <template>
-<div>
-    <div v-for="subpage in page">
-        <router-link :to="{name: subpage.link}" class="text-3xl text-white">{{subpage.name}}</router-link>
+    <div class="p-4 rounded-md bg-white w-72 m-8">
+        <div v-for="subpage in page" class="">
+        <router-link :to="{name: 'home'}" class="text-md text-black">{{subpage.name}}</router-link>
     </div>
 </div>
 </template>
