a50fb0be0f43be530b5c6f683a47c1156d0b04d0
bridgetf7 <fbridget0404@gmail.com>
2024-09-16 14:37:00 -0400

    added transition timing to subpage buttons

--- siths-redesign/src/components/SubpageMenu.vue
+++ siths-redesign/src/components/SubpageMenu.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="py-2 rounded-md bg-white w-[18vw] m-8">
+    <div class="py-2 rounded-md bg-white w-fit m-8">
       <div v-for="subpage in page" class="py-2">
-        <router-link :to="{name: 'home'}" class="rounded-md text-md text-black hover:bg-light-gray p-2 m-2">{{subpage.name}}</router-link>
+        <router-link :to="{name: 'home'}" class="rounded-md text-md text-black hover:bg-light-gray p-2 m-2 transition ease-in-out duration-300">{{subpage.name}}</router-link>
     </div>
 </div>
 </template>
