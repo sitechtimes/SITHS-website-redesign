6976785acf2594a8dd2df040092240ea17bf180d
bridgetf7 <fbridget0404@gmail.com>
2024-09-19 22:16:28 -0400

    [wip] search bar implementation for staff page
    search bar component most definitely *inspired* from course selection

---
+++ siths-redesign/src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="border-white flex justify-center items-center">
+      <div class="flex justify-center items-center relative w-5/6">
+        <input
+          :value="searchValue"
+          :placeholder="placeholder"
+          @input="$emit('update:searchValue', $event?.target?.value)"
+          class="border border-zinc-300 rounded w-full h-10 p-2 text-zinc-800"
+        />
+        
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+defineProps({
+    searchValue: String,
+    placeholder: String
+  })   
+  </script>
--- siths-redesign/src/views/StaffPage.vue
+++ siths-redesign/src/views/StaffPage.vue
@@ -1,41 +1,44 @@
 <template>
     <div class="h-fit mx-10">
-        <h1 class="font-bold w-full flex items-center justify-center text-5xl my-10">Faculty and Staff</h1>
-        <div v-for="category in categories" :key="category.value" class="mx-8">
-            <div class="flex flex-row justify-center items-center">
-                <!-- category title and seperators -->
-                <div class="w-20 h-0.5 bg-white flex-grow"></div>
-                <h2 class="text-3xl font-bold mx-2">{{ category.name }}</h2>
-                <div class="w-20 h-0.5 bg-white flex-grow"></div>
-            </div>
-            <!-- staff component -->
-            <div class="flex flex-row flex-wrap space-x-12 my-16 mx-10 justify-between">
-                <div v-for="staffMember in sortByCategory(category)" :key="staffMember._id"
-                    class="flex flex-row items-center">
-                    <img :src="staffMember.imageUrl" alt=""
-                        class="w-28 h-28 rounded-full object-cover mr-6 outline outline-white outline-2" />
-                    <div class="flex flex-col justify-center">
-                        <h2 class="text-2xl font-semibold mb-2">{{ staffMember.name }}</h2>
-                        <p class="mb-2">{{ staffMember.role }}</p>
-                        <div class="flex items-center">
-                            <img src="../assets/icons/email.png" alt="" class="w-5 mr-2">
-                            <span>
-                                <a :href="'mailto:' + staffMember.email"
-                                    class="text-white transition ease-in-out duration-300 hover:underline">
-                                    {{ staffMember.email }}
-                                </a>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+      <h1 class="font-bold w-full flex items-center justify-center text-5xl my-10">Faculty and Staff</h1>
+      <SearchBar class="w-2/3" type="text" v-model="searchInput" placeholder="Search by name or email" />
+      <div v-for="category in categories" :key="category.value" class="mx-8">
+        <div class="flex flex-row justify-center items-center">
+          <!-- category title and separators -->
+          <div class="w-20 h-0.5 bg-white flex-grow"></div>
+          <h2 class="text-3xl font-bold mx-2">{{ category.name }}</h2>
+          <div class="w-20 h-0.5 bg-white flex-grow"></div>
         </div>
+        <!-- staff component -->
+        <div class="flex flex-row flex-wrap space-x-12 my-16 mx-10 justify-between">
+          <div v-for="staffMember in sortByCategory(category)" :key="staffMember._id"
+            class="flex flex-row items-center">
+            <img :src="staffMember.imageUrl" alt=""
+              class="w-28 h-28 rounded-full object-cover mr-6 outline outline-white outline-2" />
+            <div class="flex flex-col justify-center">
+              <h2 class="text-2xl font-semibold mb-2">{{ staffMember.name }}</h2>
+              <p class="mb-2">{{ staffMember.role }}</p>
+              <div class="flex items-center">
+                <img src="../assets/icons/email.png" alt="" class="w-5 mr-2">
+                <span>
+                  <a :href="'mailto:' + staffMember.email"
+                    class="text-white transition ease-in-out duration-300 hover:underline">
+                    {{ staffMember.email }}
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</template>
+  </template>
+  
 
 <script setup>
 import { ref, onMounted } from 'vue';
 import sanityClient from '../client.js';
+import SearchBar from '@/components/SearchBar.vue';
 
 const staff = ref([]);
 const categories = ref([
@@ -43,6 +46,8 @@ const categories = ref([
     { name: 'Teachers', value: 'teachers' },
     { name: 'Other Employees', value: 'otherEmployees' },
 ]);
+
+const searchInput = ref('');
 
 const fetchStaff = async () => {
     const query = `*[_type == "staff"]{
@@ -59,6 +64,13 @@ const fetchStaff = async () => {
 
 function sortByCategory(category) {
     return staff.value.filter((staffMember) => staffMember.category === category.value);
+}
+
+function sortbySearch(searchInput){
+    if (searchInput !== " ") {
+        return staff.value.filter((staffMember) => staffMember.name.toLowerCase().includes(searchInput.toLowerCase()))
+        || staffMember.email.toLowerCase().includes(searchInput.toLowerCase());
+    }
 }
 
 onMounted(fetchStaff);
