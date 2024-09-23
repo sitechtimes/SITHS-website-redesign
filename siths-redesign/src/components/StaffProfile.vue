6601c7fcdc061e4ca3902507f2a66d1efb16d3a8
andreaguo <113132239+andreaguo@users.noreply.github.com>
2024-09-23 11:57:46 -0400

    erlenwein profile from sanity
    need to style

---
+++ siths-redesign/src/components/StaffProfile.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="h-fit mx-10">
+    <div v-for="category in categories" :key="category.value" class="mx-8">
+      <!-- staff component -->
+      <div class="flex flex-row flex-wrap space-x-12 my-16 mx-10 justify-between">
+        <div
+          v-for="staffMember in sortByCategory(category)"
+          :key="staffMember._id"
+          class="flex flex-row items-center"
+        >
+          <div v-if="staffMember.name === 'Mark Erlenwein'">
+            <img
+              :src="staffMember.imageUrl"
+              alt=""
+              class="w-28 h-28 rounded-full object-cover mr-6 outline outline-white outline-2"
+            />
+            <div class="flex flex-col justify-center">
+              <h2 class="text-2xl font-semibold mb-2">{{ staffMember.name }}</h2>
+              <p class="mb-2">{{ staffMember.role }}</p>
+              <div class="flex items-center">
+                <span>
+                  <a
+                    :href="'mailto:' + staffMember.email"
+                    class="text-white transition ease-in-out duration-300 hover:underline"
+                  >
+                    {{ staffMember.email }}
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import sanityClient from '../client.js'
+
+const staff = ref([])
+const categories = ref([
+  { name: 'Administrators', value: 'administrators' },
+  { name: 'Teachers', value: 'teachers' },
+  { name: 'Other Employees', value: 'otherEmployees' }
+])
+
+const fetchStaff = async () => {
+  const query = `*[_type == "staff"]{
+      _id,
+      name,
+      role,
+      description,
+      category,
+      "imageUrl": image.asset->url
+    }`
+
+  staff.value = await sanityClient.fetch(query)
+}
+
+function sortByCategory(category) {
+  return staff.value.filter((staffMember) => staffMember.category === category.value)
+}
+
+onMounted(fetchStaff)
+</script>
