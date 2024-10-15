036b70df174df96760c9a11d4783e31d9e69b011
kevinc2465 <chenk5104@gmail.com>
2024-10-14 21:13:11 -0400

    made a template

--- siths-redesign/src/components/SubpageAP.vue
+++ siths-redesign/src/components/SubpageAP.vue
@@ -11,8 +11,14 @@
           >
         </div>
       </div>
-      <div class="p-5 rounded-md bg-white w-[60%] m-8">
-        <h2 class="text-black font-bold text-4xl">AP English Language and Composition</h2>
+      <div class="p-7 rounded-md text-black bg-white w-[50%] m-8">
+        <h2 class="font-bold text-4xl">AP English Language and Composition</h2>
+        <h3 class="my-4 text-2xl">Taken 11th Grade</h3>
+        <p class="text-lg w-[75%]">
+          COURSE SELECTION BASED ON FOLLOWING: CLASS AVERAGES – 90% (Cumulative English Average)
+          TEACHER RECOMMENDATION – 10% 
+        </p>
+        <button class="text-white text-xl rounded-xl bg-gray w-[25%]">Description</button>
       </div>
     </div>
   </div>
