82d1a725c8f4f8f75b98e3792041902b98dfca5a
bridgetf7 <fbridget0404@gmail.com>
2024-10-24 12:00:23 -0400

    fixing formatting
    I swear I'm not farming commits

--- siths-redesign/pages/resources.vue
+++ siths-redesign/pages/resources.vue
@@ -2,6 +2,7 @@
     <div class="min-h-screen w-full px-10 flex flex-col">
         <h1 class="p-6 text-center text-5xl font-semibold">Resources</h1>
         <div class="grid grid-cols-3 gap-x-6">
-        <ResourcesCard /></div>
+            <ResourcesCard />
+        </div>
     </div>
 </template>
