1cb18f24c75223112deb00921896cd413b3fdc9a
andreaguo <113132239+andreaguo@users.noreply.github.com>
2024-10-30 11:04:20 -0400

    create page

---
+++ siths-redesign/pages/opportunities.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Special Opportunities</h1>
+      <SubpageMenu :page="clubsPages" />
+    </div>
+  </template>
+  
+  <script setup>
+  import SubpageMenu from '../components/SubpageMenu.vue'
+  const clubsPages = [
+    {
+      name: '2023-2024 Club Information',
+      url: ''
+    },
+    {
+      name: 'National Honor Society',
+      url: ''
+    },
+    {
+      name: 'Special Opportunities',
+      url: ''
+    }
+  ]
+  </script>
