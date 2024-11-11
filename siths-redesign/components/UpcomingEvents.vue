5e57921952c481025b6a3e008eb095c63a3112a1
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-11-11 13:54:37 -0500

    fix date, "contrast error"

--- siths-redesign/components/UpcomingEvents.vue
+++ siths-redesign/components/UpcomingEvents.vue
@@ -3,22 +3,23 @@
     <h1 class="my-4 text-4xl font-semibold">UPCOMING EVENTS</h1>
     <div class="flex w-full flex-col items-center justify-center lg:flex-row">
       <!-- latest 5 events -->
-      <div v-for="(event, index) in latestEvents" :key="index" class="flex flex-row items-center justify-center">
+      <div
+        v-for="(event, index) in latestEvents"
+        :key="index"
+        class="flex flex-row items-center justify-center"
+      >
         <div class="flex flex-col items-center">
           <p class="mb-2 font-semibold underline underline-offset-2">{{ event.formattedDate }}</p>
-          <div class="w-3/5 md:w-full text-center lg:w-32">
+          <div class="w-3/5 text-center md:w-full lg:w-32">
             <p>{{ event.event }}</p>
-            <p class="opacity-80 text-sm truncate">{{ event.description }}</p>
+            <p class="truncate text-sm opacity-80">{{ event.description }}</p>
           </div>
         </div>
         <!-- vertical line between each event, except for last event -->
-        <div
-          v-if="index < latestEvents.length - 1"
-          class="flex h-24 w-[1px] bg-gold mx-4"
-        ></div>
+        <div v-if="index < latestEvents.length - 1" class="mx-4 flex h-24 w-[1px] bg-gold"></div>
       </div>
     </div>
-    <NuxtLink class="btn btn-link mt-8 text-lg" to="/calendar">
+    <NuxtLink class="btn btn-link mt-8 text-lg text-gold" to="/calendar">
       Show All Events
     </NuxtLink>
   </div>
@@ -26,20 +27,22 @@
 
 <script setup>
 const websiteData = useWebsiteDataStore()
-const events = ref(websiteData.events)
 
 const latestEvents = computed(() => {
   //latest 5 events in ascending order
-  return events.value.slice(0, 5).reverse().map(event => {
-    const formattedDate = new Date(event.date).toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    });
-    return {
-      ...event,
-      formattedDate
-    }
-  })
+  return websiteData.events
+    .slice(0, 5)
+    .reverse()
+    .map((event) => {
+      const formattedDate = new Date(event.date).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      })
+      return {
+        ...event,
+        formattedDate
+      }
+    })
 })
 </script>
