6ae7fc445639255a53ce14955ca9e7e0b74dce45
Zero <113201652+kevinc2465@users.noreply.github.com>
2024-11-13 10:59:16 -0500

    starting to work on the school policy page

---
+++ siths-redesign/pages/policy.vue
@@ -0,0 +1,19 @@
+<template></template>
+<script setup>
+const websiteData = useWebsiteDataStore()
+const clubsPages = [
+  {
+    name: 'Internships'
+  },
+  {
+    name: 'Scholarships'
+  },
+  {
+    name: 'Volunteer'
+  },
+  {
+    name: 'Initiatives'
+  },
+]
+console.log(websiteData.)
+</script>
