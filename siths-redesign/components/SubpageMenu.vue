8ae72b44de96b181c392f9a8b34f0674c8a09118
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-11-17 14:55:44 -0500

    to

--- siths-redesign/components/SubpageMenu.vue
+++ siths-redesign/components/SubpageMenu.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="m-4 p-1 w-1/6 rounded-md">
+  <div class="m-4 w-1/6 rounded-md p-1">
     <ul>
-        <li v-for="subpage in pages" class="text-md text-white p-2 border-b border-white transition duration-300 ease-in-out hover:text-gold">
-          <NuxtLink :to=subpage.path>{{ subpage.name }}</NuxtLink>
-        </li>
+      <li
+        v-for="subpage in pages"
+        class="text-md border-b border-white p-2 text-white transition duration-300 ease-in-out hover:text-gold"
+      >
+        <NuxtLink :to="subpage.path">{{ subpage.name }}</NuxtLink>
+      </li>
     </ul>
   </div>
 </template>
