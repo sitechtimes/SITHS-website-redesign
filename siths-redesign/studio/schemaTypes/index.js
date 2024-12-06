be871f40341e5b6c05851f514d8dd7edf990c1a5
andreaguo <113132239+andreaguo@users.noreply.github.com>
2024-12-06 11:02:21 -0500

    asdf

--- siths-redesign/studio/schemaTypes/index.js
+++ siths-redesign/studio/schemaTypes/index.js
@@ -6,6 +6,7 @@ import {erlenwein} from './erlenwein'
 import {terrusa} from './terrusa'
 import {resources} from './resources'
 import {opportunities} from './opportunities'
+import {cdcnews} from './cdcnews'
 import { defineType } from 'sanity'
 
 
@@ -28,4 +29,4 @@ export const timeValueType = defineType({
     return times
   }
   
-export const schemaTypes = [calendar, erlenwein, schedules, staff, terrusa, timeValueType, yearlyinfo, resources, opportunities]
+export const schemaTypes = [calendar, erlenwein, schedules, staff, terrusa, timeValueType, yearlyinfo, resources, opportunities, cdcnews]
