93944963ab066a921e422a34e4db761ba09dccf0
bridgetf7 <fbridget0404@gmail.com>
2025-01-12 23:01:37 -0500

    turned resources subpage menu into filter system

--- siths-redesign/pages/resources.vue
+++ siths-redesign/pages/resources.vue
@@ -1,11 +1,29 @@
 <template>
     <div class="min-h-screen w-full px-10 flex flex-col">
         <h1 class="p-6 text-center text-5xl font-semibold">Resources</h1>
-        <div class="flex flex-row items-start">
-            <SubpageMenu :page="resourcePages" v-model="selectedChoice" />
-            <div class="grid grid-cols-3 gap-x-6">
-                <ResourcesCard :resources="filteredResources" />
+        <div class="flex flex-row justify-center space-x-6 mb-6">
+            <div class="flex-row w-full flex justify-center gap-4">
+                <label class="cursor-pointer relative text-xl"
+                    :class="{ 'underline underline-offset-2 font-bold transiiton-all duration-300': selectedChoice === 'student,staff' }">
+                    <input type="radio" v-model="selectedChoice" value="student,staff" class="hidden" />
+                    <p>All Resources</p>
+                </label>
+
+                <label class="cursor-pointer relative text-xl"
+                    :class="{ 'underline underline-offset-2 font-bold transiiton-all duration-300': selectedChoice === 'student' }">
+                    <input type="radio" v-model="selectedChoice" value="student" class="hidden" />
+                    <p>Student Resources</p>
+                </label>
+
+                <label class="cursor-pointer relative text-xl"
+                    :class="{ 'underline underline-offset-2 font-bold transiiton-all duration-300': selectedChoice === 'staff' }">
+                    <input type="radio" v-model="selectedChoice" value="staff" class="hidden" />
+                    <p>Staff Resources</p>
+                </label>
             </div>
+        </div>
+        <div class="grid grid-cols-3 gap-x-6">
+            <ResourcesCard :resources="filteredResources" />
         </div>
     </div>
 </template>
@@ -13,21 +31,16 @@
 <script setup>
 const websiteData = useWebsiteDataStore();
 
-const resourcePages = [
-    { name: 'Student and Staff Resources', value: ['student', 'staff'] },
-    { name: 'Student Resources', value: ['student'] },
-    { name: 'Staff Resources', value: ['staff'] },
-];
-
-const selectedChoice = ref(['student', 'staff']); 
+const selectedChoice = ref('student,staff');
 
 const filteredResources = computed(() => {
-    const categories = selectedChoice.value;
+    const categories = selectedChoice.value.split(',');
 
-    const filtered = websiteData.resources.filter((el) =>
-        categories.some(category => el.category.includes(category))
-    );
-
-    return filtered;
+    return websiteData.resources
+        .filter((el) =>
+            categories.some((category) => el.category.includes(category)) //'some' checks if at least one element in the array passes the test
+        )
+        .sort((a, b) => a.name.localeCompare(b.name));
 });
+
 </script>
