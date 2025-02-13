5e5051123f5eb2e52bfb00cabb803f433e982780
andreaguo <113132239+andreaguo@users.noreply.github.com>
2025-02-13 11:15:35 -0500

    custom components  for porttext

--- siths-redesign/components/PopUp.vue
+++ siths-redesign/components/PopUp.vue
@@ -5,7 +5,7 @@
         <img src="../assets/icons/x.png" alt="x" @click="selected = false">
       </button>
       <h2 class="pb-6">{{ item.name }}</h2>
-      <PortableText :value="item.description"/>
+      <PortableText :value="item.description" :components="myPortableTextComponents" />
     </div>
   </div>
 </template>
@@ -18,5 +18,33 @@ const emit = defineEmits(['closeEvent']);
 defineProps({
   item: Object
 })
+
+const myPortableTextComponents = {
+  types: {
+    image: ({ value }) => h('img', { src: value.imageUrl }),
+    callToAction: ({ value, isInline }, { slots }) =>
+      isInline
+        ? h('a', { href: value.url }, value.text)
+        : h('div', { class: 'callToAction' }, value.text),
+  },
+
+  list: {
+    // Ex. 1: customizing common list types
+    bullet: (_, { slots }) => h('li', { class: 'li list-disc' }, slots.default?.()),
+    number: (_, { slots }) => h('ol', { class: 'list-decimal' }, slots.default?.()),
+
+    // Ex. 2: rendering custom lists
+    checkmarks: (_, { slots }) => h('ol', { class: 'm-auto text-lg' }, slots.default?.()),
+  },
+
+  marks: {
+    em: (_, { slots }) => h('em', { class: 'text-red-600 font-semibold' }, slots.default?.()),
+    link: ({ value }, { slots }) => {
+      const rel = !value.href.startsWith('/') ? 'noreferrer noopener' : undefined;
+      return h('a', { class: 'text-gray underline font-semibold', href: value.href, rel }, slots.default?.());
+    },
+  },
+};
+
 
 </script>
