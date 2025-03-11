221b0e305eb0e2fb72c91a323955432f8b08ade5
DariaG16 <113132247+DariaG16@users.noreply.github.com>
2025-03-11 11:12:32 -0400

    Figured out how Tailwind works n imported gears

--- siths-redesign/pages/index.vue
+++ siths-redesign/pages/index.vue
@@ -1,11 +1,23 @@
 <template>
-  <div class="relative flex w-full flex-col items-center justify-center p-10 lg:h-screen lg:flex-row lg:p-0">
+  <div
+    class="relative flex w-full flex-col items-center justify-center p-10 lg:h-screen lg:flex-row lg:p-0"
+  >
     <!-- background gears -->
-    <div class="absolute inset-0 pointer-events-none h-full w-full">
-      <img id="left-gear" class="absolute bottom-0 left-0" src="../assets/imgs/left-gear.svg" alt=""
-        aria-hidden="true" />
-      <img id="right-gear" class="absolute bottom-0 right-0" src="../assets/imgs/right-gear.svg" alt=""
-        aria-hidden="true" />
+    <div class="pointer-events-none absolute inset-0 h-full w-full">
+      <img
+        id="left-gear"
+        class="absolute bottom-0 left-0"
+        src="../assets/imgs/left-gear.svg"
+        alt=""
+        aria-hidden="true"
+      />
+      <img
+        id="right-gear"
+        class="absolute bottom-0 right-0"
+        src="../assets/imgs/right-gear.svg"
+        alt=""
+        aria-hidden="true"
+      />
     </div>
 
     <!-- left side -->
@@ -13,17 +25,24 @@
       <!-- header texts -->
       <div id="header-texts" class="opacity-0">
         <h1 class="p-0 text-left text-gold lg:text-6xl">Welcome to</h1>
-        <h1 class="p-0 pb-4 font-medium text-left leading-tight lg:text-8xl">
-          Staten Island<br>Technical<br>High School
+        <h1 class="p-0 pb-4 text-left font-medium leading-tight lg:text-8xl">
+          Staten Island<br />Technical<br />High School
         </h1>
-        <a href="https://www.google.com/maps/place/Staten+Island+Technical+High+School/@40.5670098,-74.1177962,16.33z/data=!4m6!3m5!1s0x89c24950fa5d25c1:0x546d9c89d8bf0373!8m2!3d40.5679227!4d-74.1170347!16zL20vMDZtdGYz?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D"
-          class="flex flex-row items-center" target="_blank" rel="noopener noreferrer">
-          <img src="../assets/icons/location-pin.svg"
-            alt="Link to Staten Island Technical High School on Google Maps" />
-          <p class="text-md p-4 lg:text-lg text-white transition-all duration-300 ease-in-out hover:underline">
+        <a
+          href="https://www.google.com/maps/place/Staten+Island+Technical+High+School/@40.5670098,-74.1177962,16.33z/data=!4m6!3m5!1s0x89c24950fa5d25c1:0x546d9c89d8bf0373!8m2!3d40.5679227!4d-74.1170347!16zL20vMDZtdGYz?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D"
+          class="flex flex-row items-center"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="../assets/icons/location-pin.svg"
+            alt="Link to Staten Island Technical High School on Google Maps"
+          />
+          <p
+            class="text-md p-4 text-white transition-all duration-300 ease-in-out hover:underline lg:text-lg"
+          >
             485 Clawson St, Staten Island, NY 10306
           </p>
-
         </a>
         <!-- stats container -->
         <div id="stats-container" class="my-6 flex flex-row items-start justify-between lg:pr-10">
@@ -50,15 +69,31 @@
     </div>
 
     <!-- right side -->
-    <div class="z-10 flex h-fit w-screen flex-col justify-center px-10 lg:h-full lg:w-1/3 lg:p-10">
+    <!-- <div class="z-10 flex h-fit w-screen flex-col justify-center px-10 lg:h-full lg:w-1/3 lg:p-10"> -->
+    <div class="px-10 lg:h-full lg:w-1/3 lg:p-10">
+      <img
+        id="med-gear"
+        class="left-798px absolute bottom-auto w-[200px]"
+        src="../assets/imgs/video-gear.svg"
+        alt=""
+        aria-hidden="true"
+      />
+      <img
+        id="small-gear"
+        class="absolute bottom-auto left-[80rem] w-[280px]"
+        src="../assets/imgs/video-gear.svg"
+        alt=""
+        aria-hidden="true"
+      />
+      <img
+        id="large-gear"
+        class="absolute bottom-auto left-[60rem] w-[360px]"
+        src="../assets/imgs/video-gear.svg"
+        alt=""
+        aria-hidden="true"
+      />
       <!-- filler -->
-      <p id="right-side" class="leading-relaxed">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tincidunt orci ac urna
-        fermentum, nec facilisis magna iaculis. Proin vel lacinia turpis. Sed vehicula, odio et
-        accumsan consectetur, orci urna fringilla mi, sit amet malesuada erat lacus nec mi. Vivamus
-        aliquam libero sit amet lacus vehicula, ac viverra dolor fringilla. Fusce aliquam massa nec
-        turpis condimentum venenatis.
-      </p>
+      <!-- <p id="right-side" class="leading-relaxed"></p> -->
     </div>
   </div>
 
@@ -72,7 +107,7 @@ import { gsap } from 'gsap'
 onMounted(() => {
   gsap.to('#header-texts', {
     duration: 2,
-    opacity: 1,
+    opacity: 1
   })
 
   gsap.from('#left-gear', {
