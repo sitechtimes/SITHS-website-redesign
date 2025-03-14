8739bf7b2ebb220079dd214c6946fca8dafc5a7f
DariaG16 <113132247+DariaG16@users.noreply.github.com>
2025-03-14 11:03:57 -0400

    put the video in the gear~!

--- siths-redesign/components/VideoGears.vue
+++ siths-redesign/components/VideoGears.vue
@@ -2,22 +2,34 @@
   <div class="px-10 lg:h-full lg:w-1/3 lg:p-10">
     <img
       id="small-gear"
-      class="fill-current-(#6E6E6E) absolute bottom-auto left-[76.4rem] top-[32rem] w-[200px] rotate-[-36.34deg]"
-      src="../assets/imgs/video-gear.svg"
+      class="absolute bottom-auto left-[73rem] top-[29rem] w-[300px] rotate-[-36.34deg]"
+      src="../assets/imgs/sml-gear.svg"
       alt=""
       aria-hidden="true"
     />
+
+    <div class="relative left-[9.4rem] top-[6.3rem] h-64 w-64 overflow-hidden rounded-full">
+      <iframe
+        class="absolute inset-0 h-full w-full"
+        id="inlineFrameExample"
+        title="Inline Frame Example"
+        width="300"
+        height="200"
+        src="https://www.youtube.com/embed/u8l1VSEzhEA?loop=1&playlist=u8l1VSEzhEA&autoplay=1&mute=1&controls=0&modestbranding=1&showinfo=0&rel=0"
+      >
+      </iframe>
+    </div>
     <img
       id="med-gear"
       class="absolute bottom-auto left-[83.5rem] top-[20rem] w-[280px]"
-      src="../assets/imgs/video-gear.svg"
+      src="../assets/imgs/med-gear.svg"
       alt=""
       aria-hidden="true"
     />
     <img
       id="large-gear"
-      class="absolute bottom-auto top-[6.6rem] w-[360px] md:left-[67rem]"
-      src="../assets/imgs/video-gear.svg"
+      class="absolute bottom-auto top-[6.6rem] w-[350px] md:left-[67rem]"
+      src="../assets/imgs/lg-gear.svg"
       alt=""
       aria-hidden="true"
     />
