fc611555f3e11e1dde13c251b55a469bdadb69ed
DariaG16 <113132247+DariaG16@users.noreply.github.com>
2025-03-19 11:02:26 -0400

    added big video

---
+++ siths-redesign/components/FullVideo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="absolute left-1/2 top-1/2 h-[80vh] w-[80vw] -translate-x-1/2 -translate-y-1/2 transform overflow-hidden"
+  >
+    <iframe
+      class="inset-0 h-full w-full"
+      id="inlineFrameExample"
+      title="Inline Frame Example"
+      width="300"
+      height="200"
+      src="https://www.youtube.com/embed/u8l1VSEzhEA?loop=1&playlist=u8l1VSEzhEA&autoplay=1&mute=1&controls=0&modestbranding=1&showinfo=0&rel=0"
+    >
+    </iframe>
+  </div>
+</template>
+
+<script></script>
