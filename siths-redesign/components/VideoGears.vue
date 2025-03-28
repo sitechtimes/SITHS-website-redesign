9a2b7ab314d63cc7c06b23ec0a99097416cb2be3
DariaG16 <113132247+DariaG16@users.noreply.github.com>
2025-03-28 11:01:58 -0400

    ok they centered

--- siths-redesign/components/VideoGears.vue
+++ siths-redesign/components/VideoGears.vue
@@ -5,46 +5,41 @@
     </div>
 
     <div class="lg:h-full lg:w-1/3 lg:p-10">
-      <div id="LGearParent">
-        <div class="relative left-[9.4rem] top-[6.3rem] z-0 h-64 w-64 overflow-hidden rounded-full">
-          <iframe class="absolute inset-0 h-full w-full" :src="getVideoUrl('u8l1VSEzhEA')"></iframe>
+      <div id="LGearParent" class="relative left-[9.4rem] top-[6.3rem] mx-auto w-[350px]">
+        <div class="absolute inset-0 flex items-center justify-center">
+          <iframe class="h-[230px] w-[230px] rounded-full" :src="getVideoUrl(link1)"> </iframe>
         </div>
         <img
           id="large-gear"
-          class="absolute bottom-auto top-[6.6rem] z-10 w-[350px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105 md:left-[67rem]"
+          class="relative z-10 w-[350px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105"
           src="../assets/imgs/lg-gear.svg"
           alt=""
-          @click="selectItem('u8l1VSEzhEA')"
+          @click="selectItem(link1)"
         />
       </div>
 
-      <div id="MGearParent">
-        <div
-          class="relative left-[26rem] top-[4rem] z-0 h-[180px] w-[180px] overflow-hidden rounded-full"
-        >
-          <iframe class="absolute inset-0 h-full w-full" :src="getVideoUrl('3KePcASD0NQ')"></iframe>
+      <div id="MGearParent" class="relative left-[23rem] top-[2rem] mx-auto w-[290px]">
+        <div class="absolute inset-0 flex items-center justify-center">
+          <iframe class="h-[190px] w-[190px] rounded-full" :src="getVideoUrl(link2)"> </iframe>
         </div>
         <img
           id="med-gear"
-          class="absolute bottom-auto left-[83.5rem] top-[20rem] z-10 w-[280px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105"
+          class="relative z-10 w-[290px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105"
           src="../assets/imgs/med-gear.svg"
           alt=""
-          @click="selectItem('3KePcASD0NQ')"
+          @click="selectItem(link2)"
         />
       </div>
-
-      <div id="SGearParent">
-        <div
-          class="relative left-[17rem] top-[4rem] z-0 h-[140px] w-[140px] overflow-hidden rounded-full"
-        >
-          <iframe class="absolute inset-0 h-full w-full" :src="getVideoUrl('f7oXhDatwtY')"></iframe>
+      <div id="SGearParent" class="relative left-[17rem] top-[4rem] mx-auto w-[300px]">
+        <div class="absolute inset-0 flex items-center justify-center">
+          <iframe class="h-[140px] w-[140px] rounded-full" :src="getVideoUrl(link3)"> </iframe>
         </div>
         <img
           id="small-gear"
-          class="absolute bottom-auto left-[73rem] top-[29rem] z-10 w-[300px] rotate-[-36.34deg] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110"
+          class="relative z-10 w-[300px] rotate-[-36.34deg] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110"
           src="../assets/imgs/sml-gear.svg"
           alt=""
-          @click="selectItem('f7oXhDatwtY')"
+          @click="selectItem(link3)"
         />
       </div>
     </div>
@@ -61,6 +56,9 @@ export default {
     FullVideo
   },
   setup() {
+    const link1 = 'u8l1VSEzhEA'
+    const link2 = '3KePcASD0NQ'
+    const link3 = 'u8l1VSEzhEA'
     const videoOpen = ref(false)
     const selectedVideo = ref('')
 
@@ -78,6 +76,9 @@ export default {
     }
 
     return {
+      link1,
+      link2,
+      link3,
       videoOpen,
       selectedVideo,
       selectItem,
