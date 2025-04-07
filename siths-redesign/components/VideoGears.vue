01929ec771341247cd20cc891194489fcd145728
DariaG16 <113132247+DariaG16@users.noreply.github.com>
2025-04-07 10:24:45 -0400

    Revert "i feel like this whole home page needs to be media queried before i can properly media query gears"
    This reverts commit b2988668aa711102669001f3d08362d08c515f8b.

--- siths-redesign/components/VideoGears.vue
+++ siths-redesign/components/VideoGears.vue
@@ -5,14 +5,11 @@
     </div>
 
     <div class="lg:h-full lg:w-1/3 lg:p-10">
-      <div
-        id="LGearParent"
-        class="absolute top-[6.6rem] mx-auto w-[350px] md:left-[67rem] lg:top-[5rem] lg:w-[400px] xl:top-[4rem] xl:w-[450px]"
-      >
+      <div id="LGearParent" class="absolute top-[6.6rem] mx-auto w-[350px] md:left-[67rem]">
         <div class="absolute inset-0 flex items-center justify-center">
           <iframe
             v-if="isYouTube(link1)"
-            class="h-[230px] w-[230px] max-w-full rounded-full object-contain sm:h-[200px] sm:w-[200px] md:h-[230px] md:w-[230px]"
+            class="h-[230px] h-full max-h-full w-[230px] w-full rounded-full object-contain"
             :src="`https://www.youtube.com/embed/${getYouTubeId(link1)}?autoplay=0&mute=1&controls=0&modestbranding=1&showinfo=0&rel=0`"
             frameborder="0"
             allowfullscreen
@@ -20,7 +17,7 @@
           <video
             preload="metadata"
             v-else
-            class="h-[230px] w-[230px] rounded-full sm:h-[200px] sm:w-[200px] md:h-[230px] md:w-[230px]"
+            class="h-[230px] w-[230px] rounded-full"
             autoplay
             controls
             muted
@@ -30,21 +27,18 @@
         </div>
         <img
           id="large-gear"
-          class="relative z-10 w-[350px] rotate-[2deg] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 sm:w-[300px] md:w-[320px] lg:w-[350px] xl:w-[380px]"
+          class="relative z-10 w-[350px] rotate-[2deg] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110"
           src="../assets/imgs/lg-gear.svg"
           alt=""
           @click="selectItem(link1)"
         />
       </div>
 
-      <div
-        id="MGearParent"
-        class="absolute left-[83.5rem] top-[20rem] mx-auto w-[290px] sm:left-[50%] sm:top-[25rem] sm:w-[250px] md:left-[60%] md:top-[22rem] md:w-[270px]"
-      >
+      <div id="MGearParent" class="absolute left-[83.5rem] top-[20rem] mx-auto w-[290px]">
         <div class="absolute inset-0 bottom-[1rem] right-[1rem] flex items-center justify-center">
           <iframe
             v-if="isYouTube(link2)"
-            class="h-[190px] w-[190px] max-w-full rounded-full sm:h-[170px] sm:w-[170px] md:h-[190px] md:w-[190px]"
+            class="h-[190px] w-[190px] rounded-full"
             :src="`https://www.youtube.com/embed/${getYouTubeId(link2)}?autoplay=0&mute=1&controls=0&modestbranding=1&showinfo=0&rel=0`"
             frameborder="0"
             allowfullscreen
@@ -52,7 +46,7 @@
           <video
             preload="metadata"
             v-else
-            class="h-[190px] w-[190px] rounded-full sm:h-[170px] sm:w-[170px] md:h-[190px] md:w-[190px]"
+            class="h-[190px] w-[190px] rounded-full"
             autoplay
             controls
             muted
@@ -62,21 +56,18 @@
         </div>
         <img
           id="med-gear"
-          class="relative z-10 w-[290px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105 sm:w-[250px] md:w-[270px] lg:w-[290px] xl:w-[320px]"
+          class="relative z-10 w-[290px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105"
           src="../assets/imgs/med-gear.svg"
           alt=""
           @click="selectItem(link2)"
         />
       </div>
 
-      <div
-        id="SGearParent"
-        class="absolute left-[73rem] top-[29rem] mx-auto w-[300px] sm:left-[40%] sm:top-[30rem] sm:w-[250px] md:left-[50%] md:top-[28rem] md:w-[270px]"
-      >
+      <div id="SGearParent" class="absolute left-[73rem] top-[29rem] mx-auto w-[300px]">
         <div class="absolute inset-0 right-[1.5rem] flex items-center justify-center">
           <iframe
             v-if="isYouTube(link3)"
-            class="h-[140px] w-[140px] max-w-full rounded-full sm:h-[120px] sm:w-[120px] md:h-[140px] md:w-[140px]"
+            class="h-[140px] w-[140px] rounded-full"
             :src="`https://www.youtube.com/embed/${getYouTubeId(link3)}?autoplay=0&mute=1&controls=0&modestbranding=1&showinfo=0&rel=0`"
             frameborder="0"
             allowfullscreen
@@ -84,7 +75,7 @@
           <video
             preload="metadata"
             v-else
-            class="h-[140px] w-[140px] rounded-full sm:h-[120px] sm:w-[120px] md:h-[140px] md:w-[140px]"
+            class="h-[140px] w-[140px] rounded-full"
             autoplay
             controls
             muted
@@ -94,7 +85,7 @@
         </div>
         <img
           id="small-gear"
-          class="relative z-10 w-[300px] rotate-[-33.34deg] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105 sm:w-[250px] md:w-[270px] lg:w-[300px] xl:w-[320px]"
+          class="relative z-10 w-[300px] rotate-[-33.34deg] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105"
           src="../assets/imgs/sml-gear.svg"
           alt=""
           @click="selectItem(link3)"
