0a627cb19cb011e4604bd490639f1db4fe80dde2
Jake Deng <jaked19@nycstudents.net>
2025-04-10 11:48:42 -0400

    i will fix subpagememnu and ask andrea

--- siths-redesign/components/contactCard.vue
+++ siths-redesign/components/contactCard.vue
@@ -6,19 +6,19 @@
             <p class="absolute top-1 right-1 bg-black/45 rounded-md px-2 p-1 text-md text-gold">
                 {{ contact.season.toUpperCase() }}
             </p>
-            <div class="flex flex-row items-center">
-                <div class="w-1/3 ">
+            <div class="grid lg:flex flex-row items-center">
+                <div class="lg:w-1/3">
                     <h4 class="my-1">{{ contact.sport }}</h4>
                     <p v-for="item in contact.type"
                         class="inline bg-black/45 rounded-md px-2 py-1 my-2 text-gold text-sm">
                         {{ item.toUpperCase() }}</p>
                 </div>
-                <div class="w-1/3 my-1">
+                <div class="lg:w-1/3 my-1">
                     <p>
                         {{ contact.coach }}
                     </p>
                 </div>
-                <div class="w-1/3 my-1">
+                <div class="lg:w-1/3 my-1">
                     <p>
                         {{ contact.email }}
                     </p>
