ba5476eece03582704965e0015d30d5f57424d4b
andreaguo <113132239+andreaguo@users.noreply.github.com>
2025-04-10 10:58:15 -0400

    doneee, just need search component

--- siths-redesign/pages/athletics/contacts.vue
+++ siths-redesign/pages/athletics/contacts.vue
@@ -3,14 +3,15 @@
   <div class="flex flex-col md:flex-row justify-center items-start px-4">
     <SubpageMenu :pages="subpageLinks" :active="'Contacts'" class="absolute top-48 left-8 mb-4 md:mb-0 md:mr-8" />
     <div class="flex flex-col justify-center items-center w-1/2">
-      <input type="text" placeholder="Search" v-model="searchTerm" class="mb-6 p-2 rounded-md w-2/3">
-      <contactCard v-for="item in websiteData.athletics" title="All (A-Z)" :contacts="item.contacts"/>
+      <input type="text" placeholder="Search" v-model="searchQuery" class="mb-6 p-2 rounded-md w-2/3">
+      <contactCard v-for="item in websiteData.athletics" title="All (A-Z)" :contacts="item.contacts" />
     </div>
   </div>
 </template>
 
 <script setup>
 const websiteData = useWebsiteDataStore()
+const searchQuery = ref('')
 
 const subpageLinks = [
   {
