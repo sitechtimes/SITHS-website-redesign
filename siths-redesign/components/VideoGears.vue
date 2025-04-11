2b24207665e74fef5b94737b13129ae20260481c
DariaG16 <113132247+DariaG16@users.noreply.github.com>
2025-04-11 11:04:48 -0400

    media queries make me wanna kaeep myself SAFE

--- siths-redesign/components/VideoGears.vue
+++ siths-redesign/components/VideoGears.vue
@@ -4,8 +4,11 @@
       <FullVideo :videoSource="selectedVideo" @close="closeVideo" />
     </div>
 
-    <div class="lg:h-full lg:w-1/3 lg:p-10">
-      <div id="LGearParent" class="absolute top-[6.6rem] w-[330px] md:left-[67rem]">
+    <div class="relative min-h-[20rem] w-full lg:h-full lg:w-1/3 lg:p-10">
+      <div
+        id="LGearParent"
+        class="absolute w-[330px] sm:left-[1rem] sm:top-[0] lg:left-[67rem] lg:top-[6.6rem]"
+      >
         <div class="absolute inset-0 flex items-center justify-center">
           <img
             :src="`${thumbnail1}`"
@@ -13,16 +16,6 @@
             alt="Large Gear Thumbnail"
             @error="console.error('Thumbnail failed to load')"
           />
-          <!-- <iframe
-            v-if="isYouTube(link1)"
-            class="h-[230px] h-full max-h-full w-[230px] w-full rounded-full object-contain"
-            :src="`https://www.youtube.com/embed/${getYouTubeId(link1)}?autoplay=0&mute=1&controls=0&modestbranding=1&showinfo=0&rel=0`"
-            frameborder="0"
-            allowfullscreen
-          ></iframe>
-          <video preload="metadata" v-else class="h-[230px] w-[230px] rounded-full" controls muted>
-            <source :src="link1" type="video/mp4" />
-          </video> -->
         </div>
         <img
           id="large-gear"
@@ -33,7 +26,10 @@
         />
       </div>
 
-      <div id="MGearParent" class="absolute left-[83.5rem] top-[20rem] mx-auto w-[270px]">
+      <div
+        id="MGearParent"
+        class="absolute mx-auto w-[270px] sm:left-[20rem] sm:top-[3rem] lg:left-[83.5rem] lg:top-[20rem]"
+      >
         <div class="absolute inset-0 bottom-[1rem] right-[1rem] flex items-center justify-center">
           <img
             class="h-[180px] max-h-full rounded-full object-contain"
@@ -41,22 +37,12 @@
             alt="Medium Gear Thumbnail"
             @error="console.error('Thumbnail failed to load')"
           />
-          <!-- <iframe
-            v-if="isYouTube(link2)"
-            class="h-[190px] w-[190px] rounded-full"
-            :src="`https://www.youtube.com/embed/${getYouTubeId(link2)}?autoplay=0&mute=1&controls=0&modestbranding=1&showinfo=0&rel=0`"
-            frameborder="0"
-            allowfullscreen
-          ></iframe>
-          <video preload="metadata" v-else class="h-[190px] w-[190px] rounded-full" controls muted>
-            <source :src="link2" type="video/mp4" />
-          </video> -->
         </div>
         <img
           id="med-gear"
           class="relative z-10 w-[290px] animate-spin-slower cursor-pointer transition-transform duration-300"
           src="../assets/imgs/med-gear.svg"
-          alt=""
+          alt="Medium Gear Thumbnail"
           @click="selectItem(link2)"
         />
       </div>
@@ -69,17 +55,6 @@
             alt="Large Gear Thumbnail"
             @error="console.error('Thumbnail failed to load')"
           />
-
-          <!-- <iframe
-            v-if="isYouTube(link3)"
-            class="h-[140px] w-[140px] rounded-full"
-            :src="`https://www.youtube.com/embed/${getYouTubeId(link3)}?autoplay=0&mute=1&controls=0&modestbranding=1&showinfo=0&rel=0`"
-            frameborder="0"
-            allowfullscreen
-          ></iframe>
-          <video preload="metadata" v-else class="h-[140px] w-[140px] rounded-full" controls muted>
-            <source :src="link3" type="video/mp4" />
-          </video> -->
         </div>
         <img
           id="small-gear"
