42c05d79bc96a7a219c5a4a6bd7605bbb5d2b27c
DariaG16 <113132247+DariaG16@users.noreply.github.com>
2025-04-24 11:03:49 -0400

    how dainty and fair, how flowery and clear.
    The images on figma became code in here!

--- siths-redesign/components/ApNavbar.vue
+++ siths-redesign/components/ApNavbar.vue
@@ -1,5 +1,42 @@
 <template>
-  <div class="align-center mx-8 flex w-1/3 rounded-md bg-white p-10 text-black">hi coolios</div>
+  <div class="align-center mx-8 flex w-1/3 flex-col rounded-md bg-white p-10 text-black">
+    <div class="my-5 flex flex-row">
+      <img class="mx-5" src="../assets/icons/home.svg" alt="home icon" />
+      <h3>Welcome</h3>
+    </div>
+    <div class="my-5 flex flex-row">
+      <img class="mx-5" src="../assets/icons/selection.svg" alt="selection icon" />
+      <h3>Selection</h3>
+    </div>
+    <div class="my-5 flex flex-row">
+      <img class="mx-5" src="../assets/icons/english.svg" alt="english icon" />
+      <h3>English</h3>
+    </div>
+    <div class="my-5 flex flex-row">
+      <img class="mx-5" src="../assets/icons/russian.svg" alt="russian icon" />
+      <h3>Russian</h3>
+    </div>
+    <div class="my-5 flex flex-row">
+      <img class="mx-5" src="../assets/icons/history.svg" alt="history icon" />
+      <h3>History</h3>
+    </div>
+    <div class="my-5 flex flex-row">
+      <img class="mx-5" src="../assets/icons/science.svg" alt="science icon" />
+      <h3>Science</h3>
+    </div>
+    <div class="my-5 flex flex-row">
+      <img class="mx-5" src="../assets/icons/math.svg" alt="math icon" />
+      <h3>Math</h3>
+    </div>
+    <div class="my-5 flex flex-row">
+      <img class="mx-5" src="../assets/icons/technology.svg" alt="technology icon" />
+      <h3>Technology</h3>
+    </div>
+    <div class="my-5 flex flex-row">
+      <img class="mx-5" src="../assets/icons/rubric.svg" alt="rubric icon" />
+      <h3>Rubric</h3>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
