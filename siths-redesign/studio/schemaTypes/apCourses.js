0726da85fb7d4e911582e4aa0ce24d455743f809
DariaG16 <113132247+DariaG16@users.noreply.github.com>
2025-04-29 10:57:16 -0400

    setting up sanity

--- siths-redesign/studio/schemaTypes/apCourses.js
+++ siths-redesign/studio/schemaTypes/apCourses.js
@@ -5,87 +5,49 @@ export const apCourses = defineType({
   title: 'AP',
   type: 'document',
   fields: [
-    // defineField({
-    //   name: 'name',
-    //   title: 'Heading',
-    //   type: 'string',
-    // }),
-    // defineField({
-    //   name: 'description',
-    //   title: 'Description',
-    //   type: 'array',
-    //   of: [
-    //     {
-    //       type: 'block',
-    //       styles: [
-    //         {title: 'Normal', value: 'normal'},
-    //         {title: 'H1', value: 'h1'},
-    //         {title: 'H2', value: 'h2'},
-    //         {title: 'H3', value: 'h3'},
-    //       ],
-    //       lists: [{title: 'Numbered', value: 'number'}],
-    //       marks: {
-    //         decorators: [{title: 'Strong', value: 'strong'}],
-    //       },
-    //     },
-    //   ],
-    // }),
-    // defineField({
-    //   name: 'contacts',
-    //   title: 'Contacts',
-    //   type: 'array',
-    //   of: [
-    //     {
-    //       type: 'object',
-    //       fields: [
-    //         defineField({
-    //           name: 'sport',
-    //           title: 'Sport',
-    //           type: 'string',
-    //           description: 'ie: Badminton - Girls Varsity',
-    //         }),
-    //         defineField({
-    //           name: 'season',
-    //           title: 'Season',
-    //           type: 'string',
-    //           options: {
-    //             list: [
-    //               {title: 'Fall', value: 'Fall'},
-    //               {title: 'Winter', value: 'Winter'},
-    //               {title: 'Spring', value: 'Spring'},
-    //             ],
-    //             layout: 'dropdown',
-    //           },
-    //         }),
-    //         defineField({
-    //           name: 'coach',
-    //           title: 'Coach',
-    //           type: 'string',
-    //         }),
-    //         defineField({
-    //           name: 'email',
-    //           title: 'Email',
-    //           type: 'string',
-    //         }),
-    //         defineField({
-    //           name: 'type',
-    //           title: 'Type',
-    //           type: 'array',
-    //           of: [{type: 'string'}],
-    //           options: {
-    //             list: [
-    //               {title: 'Tryout Required', value: 'tryout required'},
-    //               {title: 'Pre-season before school', value: 'preseason'},
-    //             ],
-    //             layout: 'checkbox',
-    //           },
-    //         }),
-    //       ],
-    //     },
-    //   ],
-    //   options: {
-    //     layout: 'table',
-    //   },
-    // }),
+    defineField({
+      name: 'name',
+      title: 'Heading',
+      type: 'string',
+    }),
+    defineField({
+      name: 'explanation',
+      title: 'Explanation',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            {title: 'Normal', value: 'normal'},
+            {title: 'H1', value: 'h1'},
+            {title: 'H2', value: 'h2'},
+            {title: 'H3', value: 'h3'},
+          ],
+          lists: [{title: 'Numbered', value: 'number'}],
+          marks: {
+            decorators: [{title: 'Strong', value: 'strong'}],
+          },
+        },
+      ],
+    }),
+    defineField({
+      name: 'subnote',
+      title: 'Note',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [{title: 'Emphasis', value: 'em'}],
+          marks: {
+            decorators: [{title: 'Strong', value: 'strong'}],
+          },
+        },
+      ],
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description Link',
+      type: 'url',
+    }),
   ],
 })
