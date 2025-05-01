d81ecf80615b068d001969713b13ea78fd74ac4a
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2025-05-01 10:33:10 -0400

    Create SearchBarY.vue

---
+++ siths-redesign/components/SearchBarY.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="max-w-max">
+      <input
+        type="text"
+        placeholder="I'm searching for"
+        :value="searchInput"
+        @keyup.enter="$emit('search', searchInput.toLowerCase())"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const searchInput = ref('')
+</script>
