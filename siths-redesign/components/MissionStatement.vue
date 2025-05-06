289c52b5e0ac3f820b3e0f7f4f751b912bd3dc63
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2025-05-06 11:00:18 -0400

    Update MissionStatement.vue
    https://stackoverflow.com/questions/75271274/prevent-daisyui-carousel-from-scrolling-when-switching-slides
    
    https://github.com/saadeghi/daisyui/issues/413

--- siths-redesign/components/MissionStatement.vue
+++ siths-redesign/components/MissionStatement.vue
@@ -3,9 +3,9 @@
     <div class="flex w-full flex-col justify-center lg:flex-row">
       <!-- picture -->
       <div class="flex items-center justify-center lg:w-2/5 xl:w-1/2">
-        <div class="relative md:h-[28rem] w-full">
+        <div class="relative w-full md:h-[28rem]">
           <img
-            class="h-[15rem] md:h-full md:w-full object-cover rounded-lg"
+            class="h-[15rem] rounded-lg object-cover md:h-full md:w-full"
             src="../assets/imgs/staten-island-technical-high-school.jpg"
             alt="Staten Island Technical High School"
           />
@@ -13,15 +13,18 @@
       </div>
 
       <!-- carousel -->
-      <div class="relative flex flex-col items-center justify-center py-2 md:py-8 lg:w-3/5 xl:1/3">
+      <div
+        class="xl:1/3 relative flex flex-col items-center justify-center py-2 md:py-8 lg:w-3/5"
+        ref="carouselElement"
+      >
         <div class="carousel flex w-full items-center justify-between">
           <!-- slide 1 -->
           <div
             id="slide1"
-            class="carousel-item relative flex w-full min-h-full flex-grow items-center justify-center pb-4"
+            class="carousel-item relative flex min-h-full w-full flex-grow items-center justify-center pb-4"
           >
             <a href="#slide2" class="btn btn-circle text-xl lg:mx-6">❮</a>
-            <div class="flex mx-2 h-2/3 w-2/3 flex-col space-y-4 text-center">
+            <div class="mx-2 flex h-2/3 w-2/3 flex-col space-y-4 text-center">
               <h1 class="text-4xl font-semibold text-gray">VISION</h1>
               <p class="text-xl text-gray">
                 Staten Island Technical High School places a high priority on academics and
@@ -38,10 +41,10 @@
           <!-- slide 2 -->
           <div
             id="slide2"
-            class="carousel-item relative flex w-full min-h-full flex-grow items-center justify-center pb-4"
+            class="carousel-item relative flex min-h-full w-full flex-grow items-center justify-center pb-4"
           >
             <a href="#slide1" class="btn btn-circle text-xl lg:mx-6">❮</a>
-            <div class="flex mx-2 h-2/3 w-2/3 flex-col space-y-4 text-center">
+            <div class="mx-2 flex h-2/3 w-2/3 flex-col space-y-4 text-center">
               <h1 class="text-4xl font-semibold text-gray">MISSION</h1>
               <p class="text-xl text-gray">
                 The SITHS community fosters the development of well-rounded, passionate life-long
@@ -61,3 +64,14 @@
     </div>
   </div>
 </template>
+
+<script setup>
+import { useTemplateRef } from 'vue'
+
+const carouselElement = useTemplateRef('carouselElement')
+
+const preventScroll = (e) => {
+  e.preventDefault()
+  const button = e.currentTarget
+}
+</script>
