08d3624ec0b2ef92b8710deae545e73d70002b34
DariaG16 <113132247+DariaG16@users.noreply.github.com>
2025-06-02 11:00:58 -0400

    steal bell schedule code ig

--- siths-redesign/pages/ap-courses/english.vue
+++ siths-redesign/pages/ap-courses/english.vue
@@ -1,7 +1,9 @@
 <template>
   <h1>AP COURSES</h1>
   <div class="align m-auto flex w-[80vw] flex-row">
-    <ApNavbar :openPage="'english'" /><ApCard />
+    <ApNavbar></ApNavbar>
+    <apCard :openPage="websiteData.opportunities.filter((el) => el.opptype == 'english')" />
+    /><ApCard />
   </div>
   <!-- <InfoDropdown :posts="websiteData.posts" /> -->
 </template>
