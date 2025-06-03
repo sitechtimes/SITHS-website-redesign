d63357a978f230696ceadf925c38a6416ceb9242
DariaG16 <113132247+DariaG16@users.noreply.github.com>
2025-06-03 11:01:02 -0400

    hate. sanity. rn.
    the hatred isnt permanent i hope

--- siths-redesign/components/ApCard.vue
+++ siths-redesign/components/ApCard.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="mx-8 rounded-md bg-white p-10 text-black"></div>
+  <div class="mx-8 rounded-md bg-white p-10 text-black">
+    <div>
+      <h2 class="m-auto py-4">{{ type }}</h2>
+      <div class="py-2">
+        <div
+          v-if="apCourses.length"
+          v-for="item in apCourses"
+          :key="item.courseName"
+          class="w-7/8 rounded-md border-[1px] border-white p-8 transition-all duration-300 hover:border-gold"
+        >
+          <div>
+            <h4 class="pb-2">{{ item.courseNamename }}</h4>
+            <p class="text-sm">{{ item.description }}</p>
+          </div>
+        </div>
+        <p v-else>No courses available.</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+defineProps({
+  type: String,
+  apCourses: {
+    type: Array,
+    default: () => []
+  }
+})
+
+console.log(apCourses)
+</script>
--- siths-redesign/pages/ap-courses/math.vue
+++ siths-redesign/pages/ap-courses/math.vue
@@ -1,7 +1,27 @@
 <template>
   <h1>AP COURSES</h1>
-  <div class="align m-auto flex w-[80vw] flex-row"><ApNavbar /><ApCard /></div>
-  <!-- <InfoDropdown :posts="websiteData.posts" /> -->
+  <div class="align m-auto flex w-[80vw] flex-row">
+    <ApNavbar />
+    <ApCard
+      v-if="mathGroup && mathGroup.courses?.length"
+      :type="mathGroup.heading"
+      :apCourses="mathGroup.courses"
+    />
+    <p v-else>No AP courses found for Math.</p>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from 'vue'
+import ApNavbar from '@/components/ApNavbar.vue'
+import ApCard from '@/components/ApCard.vue'
+import { useWebsiteDataStore } from '@/stores/websiteData'
+
+const websiteData = useWebsiteDataStore()
+const mathGroup = computed(() =>
+  websiteData.apCourses.find((el) => el.heading?.toLowerCase() === 'math')
+)
+
+console.log(websiteData.apCourses)
+console.log(mathGroup.value)
+</script>
