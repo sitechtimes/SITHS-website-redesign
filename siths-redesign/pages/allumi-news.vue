7d19c9374932bee2c6e84cbcb34a4983432c5d05
jo-svg <144480253+jo-svg@users.noreply.github.com>
2025-09-18 11:02:13 -0400

    ssssssssssssssssssssss

--- siths-redesign/pages/allumi-news.vue
+++ siths-redesign/pages/allumi-news.vue
@@ -1,5 +1,7 @@
 <template>
-  <div><h1>hi</h1></div>
+  <div class="w-[80%] bg-red-200 flex flex-col justify-center"><h1>Allumi Opportunities + News</h1>
+  
+  <div class="w-[80%] bg-sky-600 "></div></div>
 </template>
 
 <script setup></script>
