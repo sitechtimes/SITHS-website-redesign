ef759455bb39030a7628ffeb3e02841676b5cc79
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2025-10-24 07:15:22 -0700

    cdc in the news schema

---
+++ siths-redesign/studio/schemaTypes/cdcNews.js
@@ -0,0 +1,32 @@
+import {defineField, defineType} from 'sanity'
+
+export const resources = defineType({
+  name: 'cdcNews',
+  title: 'CDC in the News',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'headline',
+      title: 'Article headline',
+      type: 'string',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    }),
+    defineField({
+      name: 'link',
+      title: 'Link to article',
+      type: 'url',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Article image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+})
--- siths-redesign/studio/schemaTypes/index.js
+++ siths-redesign/studio/schemaTypes/index.js
@@ -11,6 +11,7 @@ import { defineType } from 'sanity'
 import { summerHomework } from './summerHomework'
 import { directLinks } from './directLinks'
 import { athletics } from './athletics'
+import { cdcNews } from './cdcNews'
 
 
 export const timeValueType = defineType({
@@ -45,5 +46,6 @@ export const schemaTypes = [
   summerHomework, 
   partnerships, 
   directLinks, 
-  athletics
+  athletics,
+  cdcNews
 ]
