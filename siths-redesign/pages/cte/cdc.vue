51203ec2d61bc4cd0cadc94fed64b78e3ef7494e
joe <171643026+joe-238@users.noreply.github.com>
2025-11-18 22:10:28 -0500

    prettier? idk

--- siths-redesign/pages/cte/cdc.vue
+++ siths-redesign/pages/cte/cdc.vue
@@ -1,37 +1,41 @@
 <template>
-    <h1 class="text-2xl md:text-3xl lg:text-4xl text-center mb-4">Career and Technical Education</h1>
-    <div class="flex flex-col md:flex-row justify-center items-start px-4">
-        <SubpageMenu :pages="cteLinks" :active="'CDC in the News'" class="block mb-4 lg:absolute left-8 top-40"/>
-        <div class="flex items-center justify-center flex-col w-full">
-            <h2 class="text-xl md:text-2xl mb-4 md:mb-8">CDC in the News</h2>
-            <NewsArticleGrid :articles="websiteData.cdcNews"/>
-        </div>
+  <h1 class="mb-4 text-center text-2xl md:text-3xl lg:text-4xl">Career and Technical Education</h1>
+  <div class="mb-6 flex flex-col items-start justify-center px-4 md:flex-row">
+    <SubpageMenu
+      :pages="cteLinks"
+      :active="'CDC in the News'"
+      class="left-8 top-40 mb-4 block lg:absolute"
+    />
+    <div class="flex w-full flex-col items-center justify-center">
+      <h2 class="mb-4 text-xl md:mb-8 md:text-2xl">CDC in the News</h2>
+      <NewsArticleGrid :articles="websiteData.cdcNews" />
     </div>
+  </div>
 </template>
 
 <script setup>
 const websiteData = useWebsiteDataStore()
 
 const cteLinks = [
-    {
-        name: 'About',
-        path: '/cte'
-    },
-    {
-        name: "CDC in the News",
-        path: "/cte/cdc",
-    },
-    {
-        name: "Makerspace",
-        path: "/cte/makerspace",
-    },
-    {
-        name: "Certifications",
-        path: "/cte/certifications",
-    },
-    {
-        name: "Partnerships",
-        path: "/cte/partnerships",
-    }
-];
+  {
+    name: 'About',
+    path: '/cte'
+  },
+  {
+    name: 'CDC in the News',
+    path: '/cte/cdc'
+  },
+  {
+    name: 'Makerspace',
+    path: '/cte/makerspace'
+  },
+  {
+    name: 'Certifications',
+    path: '/cte/certifications'
+  },
+  {
+    name: 'Partnerships',
+    path: '/cte/partnerships'
+  }
+]
 </script>
