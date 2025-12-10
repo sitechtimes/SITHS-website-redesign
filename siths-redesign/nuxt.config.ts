14216bdb3ea864eb098408f536166cf57d5af771
Abeeha-Z <abeehaz@nycstudents.net>
2025-12-09 23:29:47 -0500

    Update nuxt.config.ts

--- siths-redesign/nuxt.config.ts
+++ siths-redesign/nuxt.config.ts
@@ -4,11 +4,6 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/main.css'],
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/sanity', '@pinia/nuxt'],
-  vite: {
-    optimizeDeps: {
-      include: ['react', 'react-dom', 'react-compiler-runtime']
-    }
-  },
   runtimeConfig: {
     sanity: {
       projectId: 'cb6mdrtg',
