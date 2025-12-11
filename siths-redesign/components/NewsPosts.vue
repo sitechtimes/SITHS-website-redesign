9d2222e553022cd31d7cfdf8a927bc0f6be269f5
Abeeha-Z <abeehaz@nycstudents.net>
2025-12-11 18:23:28 -0500

    Update NewsPosts.vue

--- siths-redesign/components/NewsPosts.vue
+++ siths-redesign/components/NewsPosts.vue
@@ -28,15 +28,12 @@
         alt="post image"
         class="mr-4 mt-2 max-h-40 min-h-24 min-w-32 max-w-64 rounded-md object-contain"
       />
-      <div v-if="post.subtitle">
+      <div>
         <p class="m-2 text-2xl font-semibold text-black">{{ post.PostTitle }}</p>
         <p v-if="post.subtitle" class="m-2 text-sm text-black">
           {{ post.subtitle }}
         </p>
       </div>
-      <p v-else class="m-2 self-center text-2xl font-semibold text-black">
-        {{ post.PostTitle }}
-      </p>
     </div>
 
     <div class="collapse-content flex flex-col justify-center">
