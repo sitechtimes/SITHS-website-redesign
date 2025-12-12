ae6e36f3ac95560d8356ced615dd81cc32d501a7
joe <171643026+joe-238@users.noreply.github.com>
2025-12-12 00:36:07 -0500

    lalallala

--- siths-redesign/pages/opportunities/index.vue
+++ siths-redesign/pages/opportunities/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <h1 class="text-2xl md:text-3xl lg:text-4xl text-center mb-4">Special Opportunities</h1>
-  <div class="flex flex-col md:flex-row justify-center items-start px-4">
-    <SubpageMenu :pages="subpageLinks" :active="'About'" class="block mb-4 lg:absolute left-8 top-40" />
-    <div class="flex items-center justify-center flex-col w-full md:w-5/6">
-      <div class="w-full md:w-3/4 lg:w-3/5">
-        <p >Learn all about SITHS' CTE programs!</p>
-      </div>
+  <h1 class="mb-4 text-center text-2xl md:text-3xl lg:text-4xl">Special Opportunities</h1>
+  <div class="flex flex-col items-start justify-center px-4 md:flex-row">
+    <SubpageMenu
+      :pages="subpageLinks"
+      :active="'About'"
+      class="left-8 top-40 mb-4 block lg:absolute"
+    />
+    <div class="flex w-full flex-col items-center justify-center md:w-5/6">
+      <p class="w-full md:w-3/4 lg:w-3/5">Learn all about SITHS' CTE programs!</p>
     </div>
   </div>
 </template>
@@ -17,21 +19,20 @@ const subpageLinks = [
     path: '/opportunities'
   },
   {
-    name: "Internships",
-    path: "/opportunities/internships",
+    name: 'Internships',
+    path: '/opportunities/internships'
   },
   {
-    name: "Scholarships",
-    path: "/opportunities/scholarships",
+    name: 'Scholarships',
+    path: '/opportunities/scholarships'
   },
   {
-    name: "Volunteer",
-    path: "/opportunities/volunteer",
+    name: 'Volunteer',
+    path: '/opportunities/volunteer'
   },
   {
-    name: "Initiatives",
-    path: "/opportunities/initiatives",
+    name: 'Initiatives',
+    path: '/opportunities/initiatives'
   }
-];
-
+]
 </script>
