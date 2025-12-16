dee22c630d5a881a839df740f8ffc442409a738c
Abeeha-Z <abeehaz@nycstudents.net>
2025-12-15 21:25:48 -0500

    movetothenewbranch

--- siths-redesign/pages/athletics/index.vue
+++ siths-redesign/pages/athletics/index.vue
@@ -1,12 +1,8 @@
 <template>
   <h1 class="mb-4 text-center text-2xl md:text-3xl lg:text-4xl">Athletics</h1>
   <div class="flex flex-col items-start justify-center px-4 md:flex-row">
-    <SubpageMenu
-      :pages="subpageLinks"
-      :active="'About'"
-      class="absolute left-8 top-48 mb-4 md:mb-0 md:mr-8"
-    />
-    <div class="flex flex-col items-center justify-center md:w-5/6">
+    <SubpageMenu :pages="subpageLinks" :active="'About'" class="left-8 top-48 z-20" />
+    <div class="z-20 flex flex-col items-center justify-center md:w-5/6">
       <h3>McKee - Staten Island Tech Public Schools Athletics League</h3>
       <p>Home of the Seagulls</p>
       <!-- psal director message -->
