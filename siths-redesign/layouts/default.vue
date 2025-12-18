8a74edf552918436b9054702b8f1d4c3578906d2
pichu1298 <kristinachuchu@gmail.com>
2025-12-18 10:22:02 -0500

    Revert "idk"
    This reverts commit f0795aad94e3d8f78c42298cf14521699b5aa937.

--- siths-redesign/layouts/default.vue
+++ siths-redesign/layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex min-h-[100dvh] flex-col">
-    <Body class="flex-1">
-      <SiteHeader />
-      <slot />
-    </Body>
-    <SiteFooter class="mt-auto" />
-  </div>
+    <div class="flex min-h-[100dvh] flex-col">
+        <Body class="flex-1">
+            <SiteHeader/>
+            <slot />
+        </Body>
+        <SiteFooter class="mt-auto" />
+    </div>
 </template>
