a14a5f44b0af6010db95bb77ba48a5f38fc86c8a
Kristina Chu <kristinachuchu@gmail.com>
2025-12-17 22:20:21 -0500

    added parent section in menu
    - no links yet
    - just added to the list

--- siths-redesign/components/SiteHeader.vue
+++ siths-redesign/components/SiteHeader.vue
@@ -1,27 +1,45 @@
 <template>
   <header>
     <!-- menu toggle -->
-    <div v-show="menuVisibility"
-      class="fixed overflow-y-scroll inset-0 bg-black bg-opacity-100 z-50">
+    <div
+      v-show="menuVisibility"
+      class="fixed inset-0 z-50 overflow-y-scroll bg-black bg-opacity-100"
+    >
       <!-- nav links -->
-      <div id="menu-content" class="absolute top-10 space-y-8 z-10 w-full">
+      <div id="menu-content" class="absolute top-10 z-10 w-full space-y-8">
         <nav class="flex flex-row flex-wrap p-8">
-          <div v-for="(category, index) in navLinks" :key="index" class="px-4 py-8 w-1/4">
-            <h3 class="mb-2 font-bold font-kumhb text-white border-b-[1px] border-gold">{{ category.category }}</h3>
+          <div v-for="(category, index) in navLinks" :key="index" class="w-1/4 px-4 py-8">
+            <h3 class="font-kumhb mb-2 border-b-[1px] border-gold font-bold text-white">
+              {{ category.category }}
+            </h3>
             <ul class="space-y-2">
               <!-- sorts navigational tabs alphabetically -->
-              <li v-for="(link, idx) in category.links.sort((a, b) => a.name.localeCompare(b.name))" :key="idx"
-                class="">
+              <li
+                v-for="(link, idx) in category.links.sort((a, b) => a.name.localeCompare(b.name))"
+                :key="idx"
+                class=""
+              >
                 <!-- NuxtLinks for in-site pages -->
-                <NuxtLink v-if="!link.directLink" :to="link.path"
-                  class="text-md font-normal hover:text-gold transition-all duration-300">
+                <NuxtLink
+                  v-if="!link.directLink"
+                  :to="link.path"
+                  class="text-md font-normal transition-all duration-300 hover:text-gold"
+                >
                   {{ link.name }}
                 </NuxtLink>
                 <!-- pages that exist outside of this site (opens in separate tab!) -->
-                <a v-else :href="link.path" target="_blank"
-                  class="text-md font-normal hover:text-gold transition-all duration-300 flex flex-row items-center">
+                <a
+                  v-else
+                  :href="link.path"
+                  target="_blank"
+                  class="text-md flex flex-row items-center font-normal transition-all duration-300 hover:text-gold"
+                >
                   {{ link.name }}
-                  <img class="inline h-5 w-5 ml-2 invert" src="../assets/icons/xlink.png" alt="Navigation leads to external site">
+                  <img
+                    class="ml-2 inline h-5 w-5 invert"
+                    src="../assets/icons/xlink.png"
+                    alt="Navigation leads to external site"
+                  />
                 </a>
               </li>
             </ul>
@@ -33,29 +51,37 @@
     <!-- menu, logo, and search -->
     <div class="flex flex-row items-center justify-between p-4 text-white">
       <!-- menu icon -->
-      <div class="z-50 w-8 h-5 flex flex-col justify-between items-center cursor-pointer" @click="toggleMenu">
-        <div :class="menuVisibility ? 'rotate-45 translate-y-2' : ''"
-          class="w-full h-1 transition-all bg-gold duration-500"></div>
-        <div :class="menuVisibility ? 'opacity-0' : ''" class="w-full h-1 transition-all duration-500 bg-gold">
-        </div>
-        <div :class="menuVisibility ? '-rotate-45 -translate-y-2' : ''"
-          class="w-full h-1 transition-all bg-gold duration-500"></div>
+      <div
+        class="z-50 flex h-5 w-8 cursor-pointer flex-col items-center justify-between"
+        @click="toggleMenu"
+      >
+        <div
+          :class="menuVisibility ? 'translate-y-2 rotate-45' : ''"
+          class="h-1 w-full bg-gold transition-all duration-500"
+        ></div>
+        <div
+          :class="menuVisibility ? 'opacity-0' : ''"
+          class="h-1 w-full bg-gold transition-all duration-500"
+        ></div>
+        <div
+          :class="menuVisibility ? '-translate-y-2 -rotate-45' : ''"
+          class="h-1 w-full bg-gold transition-all duration-500"
+        ></div>
       </div>
 
       <!-- logo -->
-      <NuxtLink to="/" class="text-3xl text-white flex flex-row items-center justify-center gap-2">
+      <NuxtLink to="/" class="flex flex-row items-center justify-center gap-2 text-3xl text-white">
         <img src="../assets/icons/animated-logo.gif" class="w-12" alt="SITHS logo" />
         SITHS
       </NuxtLink>
 
       <!-- search icon -->
       <div class="z-40 w-fit p-4">
-        <button> <img src="/assets/icons/search.svg" alt="Search website"></button>
+        <button><img src="/assets/icons/search.svg" alt="Search website" /></button>
       </div>
     </div>
   </header>
 </template>
-
 
 <script setup>
 import gsap from 'gsap'
@@ -63,32 +89,30 @@ import gsap from 'gsap'
 const menuVisibility = ref(false)
 
 const router = useRouter()
-const websiteData = useWebsiteDataStore();
+const websiteData = useWebsiteDataStore()
 
 router.afterEach(() => {
   if (menuVisibility.value) {
-    toggleMenu();
+    toggleMenu()
   }
-});
+})
 
 //pre-defined navigation links based on Nuxt pages
 const navLinks = [
   {
-    category: "Main",
-    categoryValue: "main",
-    links: [
-      { name: "Home", path: "/" },
-    ]
+    category: 'Main',
+    categoryValue: 'main',
+    links: [{ name: 'Home', path: '/' }]
   },
   {
-    category: "School Information",
-    categoryValue: "schoolinformation",
+    category: 'School Information',
+    categoryValue: 'schoolinformation',
     links: [
-      { name: "Bell Schedule", path: "/schedules", directLink: false },
-      { name: "Faculty and Staff", path: "/staff", directLink: false },
-      { name: "School Calendar", path: "/calendar", directLink: false },
-      { name: "School Year Information", path: "/yearly-info", directLink: false },
-      { name: "Mr. Terrusa's Activity Updates", path: "/terrusa", directLink: false },
+      { name: 'Bell Schedule', path: '/schedules', directLink: false },
+      { name: 'Faculty and Staff', path: '/staff', directLink: false },
+      { name: 'School Calendar', path: '/calendar', directLink: false },
+      { name: 'School Year Information', path: '/yearly-info', directLink: false },
+      { name: "Mr. Terrusa's Activity Updates", path: '/terrusa', directLink: false }
       // { name: "Communications", path: "/", directLink: false },
       // { name: "Directions", path: "/", directLink: false },
       // { name: "Special Ed/Instructional Support Services", path: "/", directLink: false },
@@ -96,64 +120,72 @@ const navLinks = [
     ]
   },
   {
-    category: "Student",
-    categoryValue: "student",
+    category: 'Student',
+    categoryValue: 'student',
     links: [
-      { name: "Principal Erlwenwein's Announcements", path: "/erlenwein", directLink: false },
-      { name: "Resources", path: "/resources", directLink: false },
-      { name: "Summer Homework", path: "/summer-homework", directLink: false },
+      { name: "Principal Erlwenwein's Announcements", path: '/erlenwein', directLink: false },
+      { name: 'Resources', path: '/resources', directLink: false },
+      { name: 'Summer Homework', path: '/summer-homework', directLink: false }
     ]
   },
   {
-    category: "Clubs and Activities",
-    categoryValue: "clubsActivities",
+    category: 'Clubs and Activities',
+    categoryValue: 'clubsActivities',
     links: [
       // { name: "Club Information", path: "/clubs", directLink: false },
-      { name: "Career and Technical Education", path: "/cte", directLink: false },
-      { name: "Special Opportunities", path: "/opportunities", directLink: false },
-      { name: "Athletics", path: "/athletics", directLink: false }
+      { name: 'Career and Technical Education', path: '/cte', directLink: false },
+      { name: 'Special Opportunities', path: '/opportunities', directLink: false },
+      { name: 'Athletics', path: '/athletics', directLink: false }
     ]
   },
   {
-    category: "Alumni",
-    categoryValue: "alumni",
+    category: 'Alumni',
+    categoryValue: 'alumni',
     links: [
-      { name: "Donations", path: "/", directLink: false },
-      { name: "News", path: "/alumniNews", directLink: false },
-      { name: "Opportunities", path: "/alumOpportunities", directLink: false },
-      { name: "Transcript Request", path: "/transcript", directLink: false }
+      { name: 'Donations', path: '/', directLink: false },
+      { name: 'News', path: '/alumniNews', directLink: false },
+      { name: 'Opportunities', path: '/alumOpportunities', directLink: false },
+      { name: 'Transcript Request', path: '/transcript', directLink: false }
     ]
   },
   {
-    category: "Contact Us",
-    categoryValue: "contactUs",
+    category: 'Contact Us',
+    categoryValue: 'contactUs',
+    links: [{ name: 'Directory', path: '/directory', directLink: false }]
+  },
+  {
+    category: 'Parents',
+    categoryValue: 'parent',
     links: [
-      { name: "Directory", path: "/directory", directLink: false },
+      { name: 'Reporting an Absence' },
+      { name: 'Student Lateness Procedures' },
+      { name: 'Absence Reporting' },
+      { name: 'Automated Telephone Message Directory' }
     ]
   }
-];
+]
 
 function addDirectLinks(directLinks) {
   // maps the direct links to their respective categories
   const mapCategories = {
-    "Main": directLinks.main || [],
-    "School Information": directLinks.schoolInformation || [],
-    "Student": directLinks.student || [],
-    "Clubs and Activities": directLinks.clubsActivities || [],
-    "Alumni": directLinks.alumni || [],
-    "Contact Us": directLinks.contactUs || []
+    Main: directLinks.main || [],
+    'School Information': directLinks.schoolInformation || [],
+    Student: directLinks.student || [],
+    'Clubs and Activities': directLinks.clubsActivities || [],
+    Alumni: directLinks.alumni || [],
+    'Contact Us': directLinks.contactUs || []
   }
 
   // iterates through the direct links and maps them to follow navLinks structure
   Object.entries(mapCategories).forEach(([category, links]) => {
-    const categoryLinks = links.map(item => ({
+    const categoryLinks = links.map((item) => ({
       name: item.title,
       path: item.link,
       directLink: true
     }))
 
     // finds the existing category and appends the direct links
-    const existingCategory = navLinks.find(link => link.category === category)
+    const existingCategory = navLinks.find((link) => link.category === category)
     if (existingCategory) {
       existingCategory.links.push(...categoryLinks)
     }
@@ -161,15 +193,19 @@ function addDirectLinks(directLinks) {
 }
 
 // ensures direct link function runs only when the data is populated
-watch(() => websiteData.directLinks, (link) => {
-  try {
-    if (link && link.length > 0) {
-      addDirectLinks(link[0])
+watch(
+  () => websiteData.directLinks,
+  (link) => {
+    try {
+      if (link && link.length > 0) {
+        addDirectLinks(link[0])
+      }
+    } catch (error) {
+      console.log(error)
     }
-  } catch (error) {
-    console.log(error)
-  }
-}, { immediate: true })
+  },
+  { immediate: true }
+)
 
 const toggleMenu = () => {
   // clicking the menu toggle will toggle the menu visibility and set overflow to prevent scrolling
@@ -193,5 +229,4 @@ const toggleMenu = () => {
     )
   }
 }
-
 </script>
