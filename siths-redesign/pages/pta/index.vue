8b4765c7fb99f2504bf41f4bfcb693d6ead775f6
Abeeha-Z <abeehaz@nycstudents.net>
2026-01-07 21:25:54 -0500

    sorryyy

--- siths-redesign/pages/pta/index.vue
+++ siths-redesign/pages/pta/index.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Home</h1>
   <div class="flex flex-col items-start justify-center px-4 md:flex-row">
-    <SubpageMenu :pages="ptaLinks" active="'About'" />
+    <SubpageMenu :pages="ptaLinks" active="About" />
     <div
       class="mb-10 flex w-full flex-col self-center rounded-lg bg-black/40 p-8 text-center md:mx-auto md:w-1/2"
     >
