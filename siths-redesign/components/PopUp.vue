704c6b418cf312798e861c20566af679b676bffd
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2026-04-15 11:01:57 -0400

    asdawd

--- siths-redesign/components/PopUp.vue
+++ siths-redesign/components/PopUp.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="absolute">
-    <div class="z-20 flex w-2/3 flex-col rounded-md bg-stone-200 p-12 text-black">
+    <div class="z-20 flex w-2/3 flex-col rounded-md bg-stone-600 p-12 text-white">
       <button @click="selected = false" class="absolute h-8 w-8 cursor-pointer self-end">
         <img src="../assets/icons/x.png" alt="x" />
       </button>
