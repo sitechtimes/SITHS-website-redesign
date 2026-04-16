807c7f6b5e7ba404bb49ee5abeac7fc8e3d62aff
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2026-04-16 11:01:27 -0400

    sdfsefsdfef

--- siths-redesign/components/PopUp.vue
+++ siths-redesign/components/PopUp.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="absolute">
-    <div class="z-20 flex w-2/3 flex-col rounded-md bg-stone-600 p-12 text-white">
+    <div class="z-20 flex w-2/3 flex-col rounded-2xl bg-stone-800 p-12 text-white">
       <button @click="selected = false" class="absolute h-8 w-8 cursor-pointer self-end">
         <img src="../assets/icons/x.png" alt="x" />
       </button>
-      <h2 class="pb-6">{{ item.name }}</h2>
+      <h2 class="pb-4">{{ item.name }}</h2>
       <PortableText :value="item.description" :components="myPortableTextComponents" />
     </div>
   </div>
